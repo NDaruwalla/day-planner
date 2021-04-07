e49ffc68b28b5b96b331af5d6810d45f66abbb71
Nicole Daruwalla <nicole.daruwalla@gmail.com>
2021-04-07 09:47:06 -0400

    created header time script using moment, developed pseudo-code

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// This is the date in the jumbotron/header using moment
+let today = moment().format('LLLL');
+$('#currentDay').text(today)
+console.log(today)
+
+// Local time needed for determining past, present, future color coding
+
+// Button behavior for saving to local storage and preserving data upon browser refresh
+
+// Change time block colors based on local time variable
