798c52e716c9dbb1cbe01d9436048b996428f4b8
Nicole Daruwalla <nicole.daruwalla@gmail.com>
2021-04-07 09:58:00 -0400

    developed html for day planner row, created local time via moment for needed for past, present, future color coding code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,14 @@
 // This is the date in the jumbotron/header using moment
 let today = moment().format('LLLL');
 $('#currentDay').text(today)
-console.log(today)
+//console.log(today)
 
 // Local time needed for determining past, present, future color coding
+let localTime = moment().format('HH');
+//console.log(localTime)
+
 
 // Button behavior for saving to local storage and preserving data upon browser refresh
 
 // Change time block colors based on local time variable
+
